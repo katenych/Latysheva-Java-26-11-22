7b857eab757bfc397de67779e65a5409e879c723
Katerina Latysheva <katenych@MBP-Katerina.attlocal.net>
2022-11-28 10:30:35 -0800

    fix mistake

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Thank you for the lesson, have a grate day!");
+        System.out.println("Thank you for the lesson, have a great day!");
     }
 }
